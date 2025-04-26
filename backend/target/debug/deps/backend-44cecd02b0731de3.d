2fb8c86b6b62cf3d7f5fb1604ebd6ec9e70ce1a8
Blindspot22 <enowscott53@gmail.com>
2025-04-26 15:03:58 +0100

    core backend structure

--- backend/target/debug/deps/backend-44cecd02b0731de3.d
+++
@@ -1,5 +0,0 @@
-/home/scott/made-in-cameroon-market/backend/target/debug/deps/libbackend-44cecd02b0731de3.rmeta: src/main.rs
-
-/home/scott/made-in-cameroon-market/backend/target/debug/deps/backend-44cecd02b0731de3.d: src/main.rs
-
-src/main.rs:
--- backend/target/debug/deps/backend-ccd6439c4a04ffa6.d
+++
@@ -1,5 +0,0 @@
-/home/scott/made-in-cameroon-market/backend/target/debug/deps/libbackend-ccd6439c4a04ffa6.rmeta: src/main.rs
-
-/home/scott/made-in-cameroon-market/backend/target/debug/deps/backend-ccd6439c4a04ffa6.d: src/main.rs
-
-src/main.rs:
